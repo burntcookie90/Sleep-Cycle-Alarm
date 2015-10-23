be73a3dabc921cb541c955e535a762b9330d1890
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2015-10-22 23:30:05 -0500

    adding unit testing

--- mobile/src/main/kotlin/io/dwak/sleepcyclealarm/view/OptionsView.kt
+++ mobile/src/main/kotlin/io/dwak/sleepcyclealarm/view/OptionsView.kt
@@ -1,7 +1,5 @@
 package io.dwak.sleepcyclealarm.view
 
-import io.dwak.sleepcyclealarm.model.Option
-
 public interface OptionsView {
     fun navigateToSleepNow()
 
---
+++ mobile/src/test/java/io/dwak/sleepcyclealarm/times/WakeUpTimesPresenterImplTest.java
@@ -0,0 +1,20 @@
+package io.dwak.sleepcyclealarm.times;
+
+import org.junit.Test;
+
+public class WakeUpTimesPresenterImplTest {
+
+    @Test
+    public void testSetSleepNow() {
+    }
+
+    @Test
+    public void testGetTimes() {
+
+    }
+
+    @Test
+    public void testWakeUpTimeSelected() {
+
+    }
+}
