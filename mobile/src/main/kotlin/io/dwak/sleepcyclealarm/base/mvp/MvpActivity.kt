8623287d0c5f5cb1e8275bccdf7ed4fd12d2fdb4
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2015-10-24 03:08:28 -0500

    do you speak it

--- mobile/src/main/kotlin/io/dwak/sleepcyclealarm/base/mvp/MvpActivity.kt
+++ mobile/src/main/kotlin/io/dwak/sleepcyclealarm/base/mvp/MvpActivity.kt
@@ -14,5 +14,16 @@ public abstract class MvpActivity<T : Presenter> : AppCompatActivity(), DaggerPr
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         inject()
+        presenter.prepareToAttachToView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.onAttachToView()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        presenter.onDetachFromView()
     }
 }
