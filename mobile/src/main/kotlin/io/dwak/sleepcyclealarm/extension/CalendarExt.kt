38bf12667112c8163d728139a41e0f7d1a66835b
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2015-10-24 18:20:50 -0500

    more calendar extension updating

--- mobile/src/main/kotlin/io/dwak/sleepcyclealarm/extension/CalendarExt.kt
+++ mobile/src/main/kotlin/io/dwak/sleepcyclealarm/extension/CalendarExt.kt
@@ -4,23 +4,14 @@ import java.util.Calendar
 import java.util.Date
 
 var Calendar.hour : Int
-    get() {
-        return get(Calendar.HOUR)
-    }
-    set(value) {
-        set(Calendar.HOUR, value)
-    }
+    get() = get(Calendar.HOUR)
+    set(value) = set(Calendar.HOUR, value)
 
 var Calendar.minute : Int
-    get() {
-        return get(Calendar.MINUTE)
-    }
-    set(value) {
-        set(Calendar.MINUTE, value)
-    }
+    get() = get(Calendar.MINUTE)
+    set(value) = set(Calendar.MINUTE, value)
 
 public fun Calendar.fromDate(date : Date) : Calendar {
-    this.time = date
-    return this
+    return apply{ time = date }
 }
 
